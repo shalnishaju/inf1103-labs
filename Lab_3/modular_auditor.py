32c9a9d33b249c080128adee99e9ad1792d2b8e8
shalni <send2shalni@gmail.com>
2026-09-24 18:14:40 +0800

    Create function calculate_tax

--- Lab_3/modular_auditor.py
+++ Lab_3/modular_auditor.py
@@ -58,5 +58,7 @@ def process_delivery(current_total, new_value):
     return new_total
 
 def calculate_tax(amount):
+    tax = amount * 0.10
+    return tax
 
 def generate_report(total_units, failed_attempts):
